bf152d86086e48dabb754cb90eade1166e536ae3
DigitalDmon <161183806+DigitalDmon@users.noreply.github.com>
2024-06-27 08:17:32 -0500

    Design phase 1

---
+++ app/src/main/java/com/example/projectharmonymusicapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.projectharmonymusicapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonSignIn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonSignIn = findViewById(R.id.button_sign_in)
+        buttonSignIn.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
