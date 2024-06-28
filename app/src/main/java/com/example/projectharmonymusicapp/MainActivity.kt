369cf0785271e3390e086385fb34e6589c96d8fa
DigitalDmon <eladio.gonzalez@ut.ac.pa>
2024-06-28 03:45:25 -0500

    Design phase 1

--- app/src/main/java/com/example/projectharmonymusicapp/MainActivity.kt
+++ app/src/main/java/com/example/projectharmonymusicapp/MainActivity.kt
@@ -11,14 +11,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var buttonSignIn: Button
+    private lateinit var buttonSignUp: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonSignIn = findViewById(R.id.button_sign_in)
+        buttonSignUp = findViewById(R.id.button_sing_up)
         buttonSignIn.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        buttonSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/projectharmonymusicapp/NavigationActivity.kt
+++ app/src/main/java/com/example/projectharmonymusicapp/NavigationActivity.kt
@@ -3,13 +3,57 @@ package com.example.projectharmonymusicapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.projectharmonymusicapp.fragments.HomeFragment
+import com.example.projectharmonymusicapp.fragments.LibraryFragment
+import com.example.projectharmonymusicapp.fragments.ProfileFragment
+import com.example.projectharmonymusicapp.fragments.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NavigationActivity : AppCompatActivity() {
+
+    private lateinit var navigationBar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        navigationBar = findViewById(R.id.bottom_navigation_view)
+        // Establishing the default fragment
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+        }
+        //Listener for the navigation bar
+        navigationBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home_frag -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.search_frag -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+                R.id.library_frag -> {
+                    replaceFragment(LibraryFragment())
+                    true
+                }
+                R.id.profile_frag -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
+    //Function to replace the fragments
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_layout_container, fragment)
+            addToBackStack(null)
+            commit()
+        }
+    }
+
 }
